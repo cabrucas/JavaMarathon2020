a237dc2e7eb3a54c6a96861f2c075c0a0aa4b3a7
Jim <dm.baevsky@gmail.com>
2020-11-30 12:39:30 +0300

    Add day1 solutions

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -2,6 +2,8 @@ package day1;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        for (int counter = 0; counter < 10; counter++) {
+            System.out.print("JAVA ");
+        }
     }
 }
--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        int counter = 0;
+        while (counter < 10) {
+            System.out.println("JAVA");
+            counter++;
+        }
     }
 }
