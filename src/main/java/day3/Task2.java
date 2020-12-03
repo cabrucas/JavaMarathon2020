95489af2a8b5b76e657cf6ba862bd25cdbf5c101
cabrucas <60257049+cabrucas@users.noreply.github.com>
2020-12-03 14:26:27 +0300

    Add day3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        double dividend, divider = 0.1d;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (divider != 0) {
+                dividend = scanner.nextDouble();
+                divider = scanner.nextDouble();
+                if (divider == 0) break;
+                System.out.println((dividend / divider));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        double dividend, divider = 0.1d;
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (int globalCounter = 0; globalCounter < 5; globalCounter++) {
+                dividend = scanner.nextDouble();
+                divider = scanner.nextDouble();
+                if (divider == 0) {
+                    System.out.println("Деление на 0");
+                    continue;
+                }
+                System.out.println(dividend / divider);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
