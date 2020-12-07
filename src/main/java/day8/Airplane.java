a8b1aab660ce19b53a42cca138f842fb16ebf807
Dmitry <dm.baevsky@gmail.com>
2020-12-07 21:42:52 +0300

    Add day 8 solution

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,80 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private double length;
+    private double weight;
+    private double fuel;
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public Airplane(String producer, int year, double length, double weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void info() {
+        System.out.println(String.format("Изготовитель: %s, год выпуска: %d, длина: %f, вес: %f, количество топлива в баке: %f",
+                producer,
+                year,
+                length,
+                weight,
+                getFuel()));
+    }
+
+    public double fillUp(double fillQuantity) {
+        return fuel += fillQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Изготовитель: %s, год выпуска: %d, длина: %.0f, вес: %.0f, количество топлива в баке: %.0f",
+                producer,
+                year,
+                length,
+                weight,
+                getFuel());
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,23 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String testStringObject = new String();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            testStringObject = testStringObject + i + " ";
+        }
+        long finishTime = System.currentTimeMillis();
+        System.out.println(testStringObject);
+        System.out.println(String.format("Затраченное время String: %d", finishTime - startTime));
 
+        StringBuilder sb = new StringBuilder();
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            sb.append(i);
+            sb.append(" ");
+        }
+        finishTime = System.currentTimeMillis();
+        System.out.println(sb.toString());
+        System.out.println(String.format("Затраченное время StringBuilder: %d", finishTime - startTime));
     }
 }
