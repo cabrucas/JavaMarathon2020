5021d2d822a8fe07e230db48668ae65b684cff43
Dmitry <dm.baevsky@gmail.com>
2020-12-17 23:00:49 +0300

    Add day 16 solution

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,49 @@
 package day16;
 
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Locale;
+
 public class Task1 {
     public static void main(String[] args) {
+        File inputFile = new File("in.txt");
+        printResult(inputFile);
+    }
 
+    public static void printResult(File file) {
+        double result = 0d;
+        int sum = 0;
+        int count = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while (reader.ready()) {
+                String[] array = reader.readLine().split(" ");
+                for (String item : array) {
+                    sum += Integer.parseInt(item);
+                    count++;
+                }
+            }
+            result = (double) sum / (double) count;
+            System.out.print(result + " --> " + round(result, 3));
+        } catch (FileNotFoundException | NumberFormatException | ArithmeticException exception) {
+           exception.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private static double round(double value, int places) {
+        BigDecimal bd = new BigDecimal(Double.toString(value));;
+        try {
+            if (places < 0) {
+                throw new IllegalArgumentException();
+            } else {
+                bd = bd.setScale(places, RoundingMode.HALF_UP);
+            }
+        } catch (IllegalArgumentException exception) {
+            exception.printStackTrace();
+        }
+        return bd.doubleValue();
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,84 @@
 package day16;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.StringJoiner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File fileNumbers = new File("file1.txt");
+        File result = new File("file2.txt");
+        final int RANDOM_MAX_VALUE = 1_000;
 
+        generateFileWithNumbers(fileNumbers, RANDOM_MAX_VALUE);
+        genereateMedians(fileNumbers, result);
+        printResult(result);
+    }
+
+    public static void generateFileWithNumbers(File file, int bound) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            Random random = new Random();
+            StringJoiner stringJoiner = new StringJoiner(" ");
+            for (int i = 0; i < 1_000; i++) {
+                stringJoiner.add(String.valueOf(random.nextInt(bound)));
+            }
+            writer.write(stringJoiner.toString());
+            writer.flush();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void genereateMedians(File fileIn, File fileOut) {
+        List<Integer> list = new ArrayList<>();
+        StringJoiner stringJoiner = new StringJoiner(" ");
+        int COUNT_ITEMS_FOR_MEDIANS = 20;
+        int sum = 0;
+        double result = 0d;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileIn));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileOut))) {
+            while (reader.ready()) {
+                String[] tmp = reader.readLine().split(" ");
+                for (String item : tmp) {
+                    list.add(Integer.parseInt(item));
+                }
+            }
+            while (list.size() > 0) {
+                for (int i = 0; i < COUNT_ITEMS_FOR_MEDIANS; i++) {
+                    sum += list.get(0);
+                    list.remove(0);
+                }
+                result = (double) sum / (double)COUNT_ITEMS_FOR_MEDIANS;
+                stringJoiner.add(String.valueOf(result));
+                sum = 0;
+            }
+            writer.write(stringJoiner.toString());
+            writer.flush();
+        } catch (FileNotFoundException | NumberFormatException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void printResult(File file) {
+        double sum = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while (reader.ready()) {
+                String[] tmp = reader.readLine().split(" ");
+                for (String item : tmp) {
+                    sum += Double.parseDouble(item);
+                }
+            }
+            System.out.println((int) sum);
+        } catch (FileNotFoundException | NumberFormatException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
